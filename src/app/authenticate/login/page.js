06181875b2235e3d020c5020a7dd6687bbfe75e5
CoolSid <hackersiddhant1@gmail.com>
2025-08-02 00:08:37 +0530

    auth security

--- src/app/authenticate/login/page.js
+++ src/app/authenticate/login/page.js
@@ -10,7 +10,7 @@ function Login() {
         <div className="text-2xl font-bold mb-2">Welcome Back to {brand_name}</div>
         <div className="mb-6 text-center">{desc}</div>
 
-        <form action="https://localhost:3000/auth/login" method="POST" className="flex flex-col w-full gap-4">
+        <form action="http://20.120.176.155/auth/login" method="POST" className="flex flex-col w-full gap-4">
           <div className="flex flex-col">
             <label htmlFor="username" className="mb-1">Username:</label>
             <input
--- src/app/authenticate/signup/page.js
+++ src/app/authenticate/signup/page.js
@@ -10,7 +10,7 @@ function Signup() {
         <div className="text-2xl font-bold mb-2">Join {brand_name}</div>
         <div className="mb-6 text-center">{desc}</div>
 
-        <form action="https://localhost:3000/auth/signup" method="POST" className="flex flex-col w-full gap-4">
+        <form action="http://20.120.176.155/auth/signup" method="POST" className="flex flex-col w-full gap-4">
           <div className="flex flex-col">
             <label htmlFor="username" className="mb-1">Username:</label>
             <input
