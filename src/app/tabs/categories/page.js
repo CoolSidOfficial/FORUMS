898978ec1d2410487c2971859693e32d8cf33700
CoolSid <hackersiddhant1@gmail.com>
2026-02-13 04:23:17 +0530

    added some icons

--- src/app/tabs/categories/page.js
+++ src/app/tabs/categories/page.js
@@ -3,36 +3,36 @@ import Link from "next/link"
 function categories() {
   
   const cat_data=[
-    {icon:"",
+    {icon:"/icons/smartphone.png",
      text:"Smartphones",
-     posts:"11",
-     members:"2000"
+     posts:"1",
+     members:"1"
     },
 
-     {icon:"",
+     {icon:"/icons/laptop.png",
      text:"Laptops",
-     posts:"11",
-     members:"2000"
+     posts:"1",
+     members:"1"
     },
- {icon:"",
+ {icon:"/icons/console.png",
      text:"Gaming",
-     posts:"11",
-     members:"2000"
+     posts:"1",
+     members:"2"
     },
- {icon:"",
+ {icon:"/icons/audio.png",
      text:"Audio",
-     posts:"11",
-     members:"2000"
+     posts:"1",
+     members:"1"
     },
- {icon:"",
-     text:"Air Purifier",
+ {icon:"/icons/af.png",
+     text:"Air Purifiers",
      posts:"11",
-     members:"2000"
+     members:"1"
     },
- {icon:"",
+ {icon:"/icons/television.png",
      text:"Televsions",
      posts:"11",
-     members:"2000"
+     members:"1"
     },
 
   ]
@@ -42,8 +42,8 @@ function categories() {
    
    <div className="grid  grid-cols-3 gap-5   ">
     {cat_data.map((each,index)=>(
-        <Link  href="" key={index} className= "bg-[#1F2937] text-xl p-6 rounded-xl h-max">
-          <span>{each.icon}</span>
+        <Link  href="" key={index} className= "bg-[#1F2937] text-large p-6 rounded-xl h-max">
+          <img src={each.icon} width={40} alt="logo" />
           <span className="text-2xl font-semibold">{each.text}</span>
         <div>Discuss {each.text} and get expert advise</div>
         <span className="m-1">
